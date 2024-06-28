f90d8fc93f48bc09ca79aade32f735ef51ccab11
DavoSegura <david.segura@sarria.salesians.cat>
2024-06-28 09:21:48 +0200

    arreglar decimales

--- caculator.v2/js/index.js
+++ caculator.v2/js/index.js
@@ -5,14 +5,6 @@ let decimalButton = document.getElementById('decimal')
 let equalButton = document.getElementById('equal')
 let plusminusButton = document.getElementById('plusminus')
 
-// ACTIVATED - NON ACTIVATED STATUS
-/*let activatedOperatorsButtons = true // At the beggining it should be off (false), overwrited to true to make the calculator functional for now.
-let activatedNumericButtons = true
-let activatedPlusMinusButton = true // At the beggining it should be off (false), overwrited to true to make the calculator functional for now.
-let activatedDecimalButton = true
-let activatedEqualButton = true // At the beggining it should be off (false), overwrited to true to make the calculator functional for now.
-let calculatorStatus = CALCULATOR.getCalculatorStatus() */
-
 addEventListeners()
 
 const CALCULATOR = new Calculator()
@@ -76,27 +68,8 @@ function addPlusMinusEventListeners () {
 }
 
 function updateDOM () {
-  //toggleStateButtonsDOM(activatedOperatorsButtons, operatorsButtons)
- /* toggleStateButtonsDOM(calculatorStatus.get('activatedOperators'), operatorsButtons)
-  toggleStateButtonsDOM(activatedNumericButtons, numbersButtons)
-  toggleStateButtonsDOM(calculatorStatus.get('activatedDecimal'), [decimalButton])
-  toggleStateButtonsDOM(activatedEqualButton, [equalButton])
-  toggleStateButtonsDOM(activatedPlusMinusButton, [plusminusButton])*/
   DISPLAY.setContent(CALCULATOR.getCurrentNumber())
 }
-
-function toggleStateButtonsDOM (state, buttons) {
-  for (let i = 0; i < buttons.length; i++) {
-    if (state) {
-      buttons[i].classList.remove('disabled')
-      buttons[i].disabled = false
-    } else {
-      buttons[i].classList.add('disabled')
-      buttons[i].disabled = true
-    }
-  }
-}
-
 
 function resetCalculatorStatus () {
   CALCULATOR.resetCalculator()
